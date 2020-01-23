cae9b4ff560806f71dd879108597375ffd56ce9b
Moritz Müller <moritzm.mueller@posteo.de>
2020-01-23 21:39:48 +0100

    refactored keyfile + password variables

--- create_account.py
+++ create_account.py
@@ -3,16 +3,18 @@ from os import path
 
 from eth_account import Account
 
+keyfile = 'account.json'
+password = ''
+
 if __name__ == '__main__':
     account = Account.create()
     print("this is my account:")
     print("adress: " + str(account.address))
     print("private: " + str(account.privateKey.hex()))
 
-    encrypted_account = account.encrypt("")
-
-    with open("account.json", "w") as file:
+    encrypted_account = account.encrypt(password)
     if path.exists:
         print('Keyfile ' + keyfile + " already exists")
         exit(1)
+    with open(keyfile, "w") as file:
         file.write(json.dumps(encrypted_account))
